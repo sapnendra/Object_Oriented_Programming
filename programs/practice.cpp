6ca640e7b1e6a8f8a910a8a46a0a5d5c66e45a30
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-11-10 22:37:02 +0530

    add modified content

--- programs/practice.cpp
+++ programs/practice.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 
-// macro with parameter
-#define Area(l, b)(l > b ? l-b : l+b)
-
 int main() {
-    int l = 25, b = 5, a;
-    
-    cout << "The Macro: " << Area(l, b) << endl;
+    int a;
+    int b;
+    a = 1;
+    b = a++;
+    cout << a <<" "<< b << endl;
     return 0;
 }
